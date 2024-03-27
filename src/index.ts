696f12eadea82dd9667f1ac6e2a8319304378ee8
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2024-03-27 14:31:47 +0600

    database setup and user migration create

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import express, { Request, Response } from "express";
-const app = express();
+import express, { Express, Request, Response } from "express";
+const app:Express = express();
 
 app.get("/", (req:Request, res:Response) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
 });
